fa181fea68e338c80f557e50292ae276e89e2b8e
sifatnabil <sifatnabil@gmail.com>
2023-11-28 00:49:01 +0600

    use openai directly instead of langchain

--- routers/travel.py
+++ routers/travel.py
@@ -1,11 +1,10 @@
 import os
+import sqlite3
+from sqlite3 import OperationalError
 
 from dotenv import dotenv_values
 from fastapi import APIRouter
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.schema import StrOutputParser
-import sqlite3
+from openai import OpenAI
 
 from models.travel import Travel
 
@@ -33,11 +32,10 @@ async def should_travel(travel: Travel) -> None:
     os.environ["OPENAI_API_KEY"] = config["OPENAI_API_KEY"]
 
     # Create the OpenAI chatbot
-    llm = ChatOpenAI(temperature=0)
+    client = OpenAI()
 
     # Define the prompt
-    prompt = PromptTemplate.from_template(
-        """
+    prompt = """
         Write Exactly two SQl queries to get the temperature of the source and destination.
 
         Query1: SELECT temperature FROM weather WHERE name = {source};
@@ -46,12 +44,52 @@ async def should_travel(travel: Travel) -> None:
         return the output in the following format:
         (Query1, Query2)
         """
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": f"{source}, {destination}"}
+        ],
+        temperature=0
     )
 
-    # Create the Prompt Flow
-    runnable = prompt | llm | StrOutputParser()
+    queries = response.choices[0].message.content.replace("(", "").replace(")", "")
+    source_query, destination_query = queries.split(",")
 
-    # Run the Prompt Flow
-    result = runnable.invoke({"source": source, "destination": destination})
+    # Create connection to the SQLite database
+    con = sqlite3.connect("data/weather.db")
 
+    # Create cursor
+    cur = con.cursor()
 
+    try:
+        # Execute the generated queries
+        cur.execute(source_query)
+        res = cur.fetchone()
+        if res is not None:
+            source_temp = res
+        else:
+            source_temp = None
+
+        cur.execute(destination_query)
+        res = cur.fetchone()
+        if res is not None:
+            dest_temp = res[0]
+        else:
+            dest_temp = None
+
+        con.close() # Close the database connection
+
+    except OperationalError:
+        con.close() # Close the database connection
+        return "Query Generation Failed!"
+
+    # Check the condition to decide whether the user should travel or not
+    if source_temp is None and dest_temp is not None:
+        return "Yes You should!"
+    if source_temp is not None and dest_temp is None:
+        return "No You shouldn't 😟"
+
+    return "District Information is not in the Database"
+    
